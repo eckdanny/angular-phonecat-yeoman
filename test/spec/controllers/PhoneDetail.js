f1fb4c33fd705b821bbd9db03e94c3a9799ef32c
Danny Eck <eck.danny@gmail.com>
2013-09-24 19:22:52 -0500

    minor file name change

--- test/spec/controllers/PhoneDetail.js
+++ test/spec/controllers/PhoneDetail.js
@@ -16,7 +16,7 @@ describe('Controller: PhoneDetailCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  xit('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
 });
