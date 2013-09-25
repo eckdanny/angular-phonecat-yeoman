08435d12989018116806a3040976e48c9ffc1fa8
Danny Eck <eck.danny@gmail.com>
2013-09-24 20:01:29 -0500

    completed phonecat port

--- test/spec/controllers/PhoneDetail.js
+++ test/spec/controllers/PhoneDetail.js
@@ -2,21 +2,50 @@
 
 describe('Controller: PhoneDetailCtrl', function () {
 
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
   // load the controller's module
   beforeEach(module('demoApp'));
 
   var PhoneDetailCtrl,
-    scope;
+    scope,
+    $httpBackend,
+    $routeParams;
+
+  var xyzPhoneData = function() {
+    return {
+      name: 'phone xyz',
+          images: ['image/url1.png', 'image/url2.png']
+    }
+  };
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_, $routeParams) {
+
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
+
+    $routeParams.phoneId = 'xyz';
+
     scope = $rootScope.$new();
+
     PhoneDetailCtrl = $controller('PhoneDetailCtrl', {
       $scope: scope
     });
+
   }));
 
-  xit('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should fetch phone detail', function() {
+    expect(scope.phone).toEqualData({});
+    $httpBackend.flush();
+
+    expect(scope.phone).toEqualData(xyzPhoneData());
   });
+
 });
--- test/spec/controllers/phones.js
+++ test/spec/controllers/phones.js
@@ -2,21 +2,39 @@
 
 describe('Controller: PhonesCtrl', function () {
 
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
   // load the controller's module
   beforeEach(module('demoApp'));
 
   var PhonesCtrl,
-    scope;
+    scope,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('phones/phones.json').respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     scope = $rootScope.$new();
     PhonesCtrl = $controller('PhonesCtrl', {
       $scope: scope
     });
   }));
 
-  xit('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should create "phones" model with 2 phones fetched from xhr', function() {
+    expect(scope.phones).toEqualData([]);
+    $httpBackend.flush();
+    expect(scope.phones).toEqualData([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
   });
+
+  it('should set the default value of orderProp model', function() {
+    expect(scope.orderProp).toBe('age');
+  });
+
 });
